e7961913bfb9aebf2f04e949e9fa0026a2cf9193
mans <mans@pop-os.localdomain>
2023-07-28 22:54:51 +0200

    did a lot of stuff

--- main.cpp
+++ main.cpp
@@ -41,8 +41,7 @@ int main(int, char**){
     };
 
     // setting up for raytracing
-    float step = 0.2f;
-    int max_steps = 400;
+    int max_ray_dist = 100.0f;
     float z0 = 0.5f / glm::tan(fov/2);
 
     bool running = true;
@@ -64,13 +63,18 @@ int main(int, char**){
             for (int j = 0; j < wtw; j++) {
                 
                 glm::vec3 start_pos = glm::vec3(j/wtw-0.5f - p.x, i/wth-0.5f, z0);
-                glm::vec3 step_vec = (rot_mat * glm::vec4(glm::normalize(start_pos - p), 0.0f)) * step;
+                glm::vec3 norm_vec = (rot_mat * glm::vec4(glm::normalize(start_pos - p), 0.0f));
 
                 glm::vec3 p1 = start_pos;
                 glm::vec3 p2 = start_pos;
 
+                bool stopped = false;
+
+                // kolla
+
+                /*
                 int steps = 0;
-                while (steps < max_steps) {
+                while (steps < max_steps && (!stopped)) {
 
                     p1 = p2;
                     p2 += step_vec;
@@ -78,10 +82,34 @@ int main(int, char**){
                     // check if the line between p1 and p2 intersects with any triangle
                     for (int ti = 0; ti < vertices.size()/3; ti += 3) {
                         
-                        
-                        glm::vec3 normal = glm::cross(vertices[ti] - vertices[ti+1], vertices[ti] - vertices[ti+2]);
+                        // borde räkna ut surface matriserna innan detta så att det endast behövs göras en gång
+
+                        // los computaciones caros
+                        glm::vec3 axis1 = vertices[ti] - vertices[ti+1];
+                        glm::vec3 normal = glm::cross(axis1, vertices[ti] - vertices[ti+2]);
+                        glm::vec3 axis2 = glm::cross(axis1, axis2);
+
+                        glm::mat4 surface_mat = glm::mat4(glm::vec4(normal, 0.0f), glm::vec4(axis1, 0.0f), glm::vec4(axis2, 0.0f), glm::vec4(0.0f));
+                        glm::mat4 surface_inverse = glm::inverse(surface_mat);
+
+                        float triangle_normal_pos = (surface_inverse * glm::vec4(vertices[ti], 0.0f)).x;
+
+                        float p1_normal_pos = (surface_inverse * glm::vec4(p1, 0.0f)).x;
+                        float p2_normal_pos = (surface_inverse * glm::vec4(p2, 0.0f)).x;
+
+                        if ((triangle_normal_pos - p1_normal_pos < 0) != (triangle_normal_pos - p2_normal_pos)) {
+                            stopped = true;
+                            break;
+                        }
 
                     }
+
+                }
+                */
+
+                if (stopped) {
+
+                    // do essentially the same process, but from the position where the ray was stopped, to every light source
 
                 }
 
@@ -101,3 +129,5 @@ int main(int, char**){
 
 // plan: modellera programmet utan användningen av shaders först, för att sedan börja använda OpenGL för renderingen. Blir nog enklast om jag bara börjar rendera skärmen på en
 // texture i SDL2. 
+
+// TODO: ha så att ljuset av ett fragment beror på fragmentets distans till ljuskällor och till kameran
